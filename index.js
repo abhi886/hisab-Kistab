a3ff62efc0d2d6778e58423c7cd17c396fe5706a
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-01-07 12:19:21 +1100

    Add Environment variable PORT

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) =>{
     res.send('hello world');
 });
 
-
-app.listen(3000, ()=> console.log('Listening to port 3000'));
+const port = process.env.PORT || 3000;
+app.listen(3000, ()=> console.log(`Listening to port ${port}`));
