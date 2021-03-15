3692c318bdf0e0287ee6e65ee1ab39524140b807
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-03-15 18:25:49 +1100

    Change Mongodb uri

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 // Changed on jan 5
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Connect to MongoDB. This is just a test.
+// Would be removed after the real connection to the mongodb.
+mongoose
+  .connect('mongodb://algo-app-mongodb:27017/test', { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => {
+    console.log('error found');
+    console.log(err);
+  });
 
 app.get('/', (req, res) => {
   res.send('hello world');
