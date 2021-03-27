7134f8b08701b32642963abc98dc42beb91bebdd
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-03-27 21:57:36 +1100

    Use lodash to make response more simpler

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable consistent-return */
+const _ = require('lodash');
 const mongoose = require('mongoose');
 const express = require('express');
 const { User, validate } = require('../models/user');
@@ -11,13 +12,8 @@ router.post('/', async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User exists Damit');
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    isAdmin: req.body.isAdmin,
-  });
+  user = new User(_.pick(req.body, ['name', 'email', 'password', 'isAdmin']));
   await user.save();
-  res.send(user);
+  res.send(_.pick(user, ['name', 'email']));
 });
 module.exports = router;
