469cbca2e5b3797b01afbc3408ba7ef2ba112015
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-03-27 20:43:11 +1100

    Registering Users

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 // Changed on jan 5
 const express = require('express');
 const mongoose = require('mongoose');
+const users = require('../routes/users');
 
 const app = express();
 
 // Connect to MongoDB. This is just a test.
 // Would be removed after the real connection to the mongodb.changes
 mongoose
-  .connect('mongodb://algo-app-mongodb:27017/test', { useNewUrlParser: true })
+  .connect('mongodb://localhost/test', { useNewUrlParser: true })
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => {
     console.log('error found');
     console.log(err);
   });
+app.use(express.json());
+app.use('/api/users', users);
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send('hellooo world');
 });
 
 const port = process.env.PORT || 3000;
