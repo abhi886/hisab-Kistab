bd795b76f5d344906cf561b95aa32d41219b414c
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-05-12 13:27:14 +1000

    Modify Delete Member function to async

--- routes/tests.js
+++ routes/tests.js
@@ -1,8 +1,35 @@
+const readline = require('readline');
 const express = require('express');
+const { User } = require('../models/user');
+const { url } = require('inspector');
 
 const router = express.Router();
-router.get('/', async () => {
-  console.log('reached here');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+router.get('/example1', async () => {
+  console.log(
+    'Testing asynchronous function with readline module and its awesome'
+  );
+  rl.question('How do you like Node ??', (answer) => {
+    console.log(`Your answer: ${answer}`);
+  });
+  console.log('Execute before');
+});
+
+router.get('/example2', async () => {
+  const a = ['a1', 'a2', 'a3'];
+  try {
+    for (let i = 0; i < a.length; i += 1) {
+      const b = a[i];
+      const result = await User.findOne();
+      console.log(`${b}: ${result.name}`);
+    }
+    console.log('Done the loop');
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 module.exports = router;
